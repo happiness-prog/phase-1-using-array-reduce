0e59a30a74b09b55e9e9845c7249a28bb69e78cd
Happiness Maina <mainahappiness361@gmail.com>
2024-12-11 10:04:16 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Code your solution here 
+const totalBatteries=batteryBatches.reduce(sumOfBatteries);
+
+function sumOfBatteries(acc, curr){
+    return acc +curr
+};
